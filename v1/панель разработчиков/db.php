a05896fd05fd544463ec2c4e34dc72e6b27f1a86
Azamat Ibragimov <ibrprofile@bk.ru>
2024-11-17 18:03:53 +0300

    Update db.php

--- v1/панель разработчиков/db.php
+++ v1/панель разработчиков/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = '85.193.87.122';
-$db = 'tesamarket';
-$user = 'tesamarket';
-$pass = 'Ggjksu29-=';
+$host = '';
+$db = '';
+$user = '';
+$pass = '';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=utf8";
 $pdo = new PDO($dsn, $user, $pass);
